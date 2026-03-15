02f4f39d0979748230c0815ab01f6ae6e34a5d5a
Goveend Kumar <goveend.kumar02@gmail.com>
2026-03-14 23:38:14 -0700

    Add files via upload

---
+++ BasicProgram.py
@@ -0,0 +1,32 @@
+#1. Print the message "Hello World" on the screen?
+# Line Of Code 
+print("Hello World")
+
+#2. Find the sum of two numbers entered by the user?
+# Line Of Code 
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+ 
+sum = a + b
+print("Sum is:", sum)
+
+#3. Find the square of a number:
+# Line Of Code 
+num = int(input("Enter a number "))
+square = num * num
+print("Square is:", square)
+
+#4. Calculate the Area of a Circle?
+# Line Of Code
+radius = float(input("Enter ralius "))
+area = 3.14 * radius * radius
+print("Area of circle: ", area)
+
+#5. Swap the value of two numbers?
+# Line Of Code
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+a,b = b,a
+print("After Swapping numbers")
+print("a =", a)
+print("b =",b)
